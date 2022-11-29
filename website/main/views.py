e563ffed28159876f47a7ff4439d933382086cea
svetl <v.svetlichniy1999@gmail.com>
2022-11-29 20:20:58 +0300

    Added urls and views

--- website/main/views.py
+++ website/main/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+
+def index(request):
+    return HttpResponse('<h4>Hello</h4>')
+
+
+def page_1(request):
+    return HttpResponse('<h1>This is page_1</h1>')
