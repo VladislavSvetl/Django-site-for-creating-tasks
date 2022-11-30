49aafd364e0319676d060ee9d84ec48e9e380ab5
svetl <v.svetlichniy1999@gmail.com>
2022-11-30 23:13:13 +0300

    Added model: class 'Task'

--- website/main/models.py
+++ website/main/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    number = models.CharField('Номер по порядку', max_length=10)
+    title = models.CharField('Название', max_length=50)
+    task = models.TextField('Описание')
+    category = models.CharField('Категория', max_length=25)
+    date_time = models.DateTimeField('Дата и время')
+
+    def __str__(self):
+        return self.title
